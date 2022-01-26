bf460bd8412cc6962d2b4edfb9a671bfe4cec455
Olexii Nesterets <aleksejnesterec@dotsplatform.com>
2022-01-26 17:26:32 +0200

    Add websocket and style refactor

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 'use strict';
 
-(function() {
+(function () {
     const switchBetween = (selector1, selector2) => {
         document.querySelector(selector1).classList.toggle('hidden');
         document.querySelector(selector2).classList.toggle('hidden');
     };
 
-    const handleClick = () => {
-        const date = new Date();
+    const pipe = (val) => (...fns) => fns.reduce((acc, fn) => fn(acc), val);
+    const callMethod = (methodName) => (val) => val[methodName]();
+    const addLeadingZero = (val) => val.length < 2 ? `0${val}` : val;
+    const addTrailingZero = (val) => val.length < 3 ? addTrailingZero(`${val}0`) : val;
 
-        document.querySelector('.hours').textContent = date.getHours();
-        document.querySelector('.minutes').textContent = date.getMinutes();
-        document.querySelector('.seconds').textContent = `${date.getSeconds()}.${date.getMilliseconds()}`;
+    const getTimeRowHtml = (date) => {
+        const datePipe = pipe(date);
+        const hours = datePipe(callMethod('getHours'), callMethod('toString'), addLeadingZero);
+        const minutes = datePipe(callMethod('getMinutes'), callMethod('toString'), addLeadingZero);
+        const seconds = datePipe(callMethod('getSeconds'), callMethod('toString'), addLeadingZero);
+        const milliseconds = datePipe(callMethod('getMilliseconds'), callMethod('toString'), addTrailingZero);
+        return `
+            <span class="time">
+                ${hours}<span class="colon">:</span>${minutes}<span class="colon">:</span>${seconds}<span class="dot">.</span>${milliseconds}
+            </span>
+        `;
+    }
+
+    const handleClick = () => {
+
+        const time = new Date();
+
+        const makeId = (length, acc = '') => {
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            const getRandomNumberFrom = (length) => () => Math.floor(Math.random() * length);
+            const getRandomFromCharsLength = getRandomNumberFrom(characters.length);
+            const id = acc + characters.charAt(getRandomFromCharsLength());
+            return (id.length === length) ? id : makeId(length, id);
+        }
+
+        const id = makeId(64);
+
+        const socket = new WebSocket(`wss://damp-basin-24026.herokuapp.com/`);
+
+        socket.addEventListener('open', function () {
+            socket.send(JSON.stringify({ id, time }));
+        });
+
+        socket.addEventListener('message', function (event) {
+
+            let dataArr = JSON.parse(event.data);
+
+            const list = dataArr.reduce((acc, elem, i) => {
+                const div = document.createElement('div');
+                div.classList.add('time-row');
+                div.innerHTML = `<span class="number-sign">№</span><span>${i + 1}</span> <span class="delimiter"></span> ${getTimeRowHtml(new Date(elem.time))}`;
+                if (elem.id === id) {
+                    div.classList.add('highlighted');
+                }
+                acc.push(div);
+                return acc;
+            }, []);
+
+            document.querySelector('.list').innerHTML = '';
+            document.querySelector('.list').append(...list);
+        });
 
         switchBetween('.show-time-btn', '.time-info');
     }
